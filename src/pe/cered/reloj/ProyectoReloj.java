17f0a307b4f2b5fea808e33370108d078a042dbb
Andrade <Andrade@DESKTOP-6M2RKMO>
2023-12-20 15:01:13 -0500

    Creando Reloj

---
+++ src/pe/cered/reloj/ProyectoReloj.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pe.cered.reloj;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Andrade
+ */
+public class ProyectoReloj {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        Date reloj;
+        DateFormat df = new SimpleDateFormat("hh:mm:ss");
+       
+        while(true){
+            reloj = new Date();
+            System.out.println(df.format(reloj));
+            
+            Thread.sleep(1000);
+        }
+        
+        
+        
+        
+    }
+    
+}
